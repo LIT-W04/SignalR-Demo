57e9fbaeec598a115ce82adf404829aeb6bbd0a6
Avrumi Friedman <info@lakewoodprogramming.com>
2017-08-08 20:24:43 -0400

    Initial commit

---
+++ SignalRDemo/Scripts/index.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+
+    var hub = $.connection.simpleHub;
+//    $.connection.hub.start().done(function() {
+//        hub.server.connected();
+    //    });
+
+    $.connection.hub.start();
+
+    $("#send-message").on('click', function() {
+        hub.server.newMessage($("#name").val(), $("#message").val());
+        $("#message").val('');
+    });
+
+    hub.client.newMessagePosted = function(message) {
+        $("#chat-area").append(`${message.Name}: ${message.Message} \r\n`);
+    }
+
+    hub.client.newConnection = function(msg) {
+        $("#chat-area").append(msg + "\r\n");
+    }
+
+
+});
---
+++ SignalRDemo/SimpleHub.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo
+{
+    public class SimpleMessage
+    {
+        public string Name { get; set; }
+        
+        public string Message { get; set; }
+    }
+
+    public class SimpleHub : Hub
+    {
+        private static int _count = 0;
+        public void Connected()
+        {
+            //Context.User.Identity.IsAuthenticated
+            //Context.User.Identity.Name
+            _count++;
+            Clients.All.newConnection($"User {_count} connected");
+        }
+
+        public void NewMessage(string name, string message)
+        {
+            Clients.All.newMessagePosted(new SimpleMessage
+            {
+                Name = name, Message = message
+            });
+        }
+    }
+}
